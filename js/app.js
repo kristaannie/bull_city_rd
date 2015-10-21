75ac0cba3692820c53d103af5ba12b323589efcd
Krista Anne Nordgren <kristaanne9@Kristas-MacBook-Pro.local>
2015-10-21 17:39:55 -0400

    Update 2015-10-21T21:39:47.899Z

---
+++ js/app.js
@@ -0,0 +1,2 @@
+var app={};$(function(){console.log("ready!"),$(".outer-modal").hide(),$(".x-button").click(function(){$(".outer-modal").hide()}),$("button").on("click",function(){$(".outer-modal").fadeIn("slow","swing")}),$(".practice").click(function(){$(".outer-modal").show(200),$(".practice-content").show(),$(".learn-more-content").hide()}),$(".learn-more").click(function(){$(".outer-modal").show(),$(".learn-more-content").show(),$(".practice-content").hide()})});
+//# sourceMappingURL=app.js.map
